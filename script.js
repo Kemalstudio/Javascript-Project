95ea8062160c270192343bebced6546f7a5aefd6
Kemal Atayev <kemalatayew913@gmail.com>
2025-12-04 20:53:49 +0500

    1.29

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
-let = cartHtml
+let favoriteColor;
 
-cartHtml = `
-<div class="cart-container">
-  <h2>Your Shopping Cart</h2>
-  <div class="cart-items">`
-cartHtml += `
-    <div class="cart-item">
-      <span class="item-name">Product 1</span>
-        <span class="item-quantity">2</span>
-        <span class="item-price">$20.00</span>
-    </div>
-    <div class="cart-item">
-      <span class="item-name">Product 2</span>
-        <span class="item-quantity">1</span>
-        <span class="item-price ">$15.00</span>
-    </div>
-`
-
-cartHtml += `
-    </div>
+prompt("First prompt");
+prompt("Second prompt");
