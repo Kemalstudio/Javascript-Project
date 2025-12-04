33bfb0f8641b9cd4b883421d9415f522934c38e1
Kemal Atayev <kemalatayew913@gmail.com>
2025-12-04 23:35:40 +0500

    1.55

--- 02/script.js
+++ 02/script.js
@@ -24,4 +24,4 @@ switch (weekday) {
         break;
 }
 
-// constweekday = 'sunda'; case monday, tuesday, wednesday, thursday, friday, s
+// constweekday = 'sunda'; case monday, tuesday, wednesday, thursday, friday, 
