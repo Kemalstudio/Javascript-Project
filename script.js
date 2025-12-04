1a6d80c49126e6f73de8cc381650318e1490057c
Kemal Atayev <kemalatayew913@gmail.com>
2025-12-04 19:43:40 +0500

    1.26

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-const firstNumber = 50;
-const secondNumber = 10;
+let = cartHtml
 
-// SImple math ope
-result = firstNumber + secondNumber;
-result = firstNumber - secondNumber;
+cartHtml = `
+<div class="cart-container">
+  <h2>Your Shopping Cart</h2>
+  <div class="cart-items">`
+cartHtml += `
+    <div class="cart-item">
+      <span class="item-name">Product 1</span>
+        <span class="item-quantity">2</span>
+        <span class="item-price">$20.00</span>
+    </div>
+    <div class="cart-item">
+      <span class="item-name">Product 2</span>
+        <span class="item-quantity">1</span>
+        <span class="item-price ">$15.00</span>
+    </div>
+`
 
-
-
-console.log(result)
+cartHtml += `
