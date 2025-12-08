578c6e32818157bf26392c3e7e9a52ba3f65a0d6
Kemal Atayev <kemalatayew913@gmail.com>
2025-12-08 15:48:20 +0500

    3.10

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ const data1 = [49, 51, 63];
 const data2 = [31, 29, 43, 58, 52];
 
 const printHumiditiesForecast = function(arr) {
-    
-    for (let i =0;  i < arr.length; i++) {
+    let str = '';
 
+    for (let i =0;  i < arr.length; i++) {
+        str =str + `${arr[i]}%`;
     }
+
+    console.log(str);
+    
 }
+
+printHumiditiesForecast(data1);
