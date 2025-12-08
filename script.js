c162e48119459adc31b6cfa6b529bfec4d8278ce
Kemal Atayev <kemalatayew913@gmail.com>
2025-12-08 22:18:44 +0500

    3.32

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 'use strict';
 
 console.log(document.querySelector('.quess-message').textContent); 
+
+document.querySelector('.quess-message').textContent = 'Правильно!';
+
+document.querySelector('.question').textContent = 7;
+
+document.querySelector('.score').textContent = 11;
