9293d21a35f9548e82f2d817a1b241d07a3b96c1
Kemal Atayev <kemalatayew913@gmail.com>
2025-12-08 22:34:36 +0500

    3.35

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 'use strict';
 
-console.log(document.querySelector('.guess-message').textContent); 
+// console.log(document.querySelector('.guess-message').textContent); 
 
-document.querySelector('.guess-message').textContent = 'Правильно!';
+// document.querySelector('.guess-message').textContent = 'Правильно!';
 
-document.querySelector('.question').textContent = 7;
+// document.querySelector('.question').textContent = 7;
 
-document.querySelector('.score').textContent = 11;
+// document.querySelector('.score').textContent = 11;
 
-console.log(document.querySelector('.number-input').value);
+// console.log(document.querySelector('.number-input').value);
 
-document.querySelector('.number-input').value = 13;
+// document.querySelector('.number-input').value = 13;
 
-console.log(document.querySelector('.number-input').value);
+// console.log(document.querySelector('.number-input').value);
+
+
+document.querySelector('.check').addEventListener('click', function() {
+    console.log(document.querySelector('.number-input').value);
+    
+})
