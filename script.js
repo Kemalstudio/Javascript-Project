c122874dedd028e3688e5fcebd32a6605f57e45b
Kemal Atayev <kemalatayew913@gmail.com>
2025-12-09 17:53:00 +0500

    3.73

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ document.querySelector('.check').addEventListener('click', function () {
   if (!guess) {
     document.querySelector('.guess-message').textContent = 'Нет числа!';
 
-    // Когда игрок
+    // Когда игро
   } else if (guess === secretNumber) {
     document.querySelector('.guess-message').textContent = 'Правильно!';
     document.querySelector('.question').textContent = secretNumber;
