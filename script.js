573b428085939fef339b6a28040e98a8d01b437f
Kemal Atayev <kemalatayew913@gmail.com>
2025-12-09 15:46:11 +0500

    3.49

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.question').style.width = '50rem';
 
-    // Обновляем лучший результ
+    // Обновляем лучший резуль
     if (score > highscore) {
       highscore = score;
       document.querySelector('.highscore').textContent = highscore;
