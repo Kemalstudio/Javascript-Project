88b8f4bbb4c1eb165ee9777bbf7640faf0a46c4e
Kemal Atayev <kemalatayew913@gmail.com>
2025-12-09 19:57:31 +0500

    4.1

--- Game/script.js
+++ Game/script.js
@@ -3,6 +3,7 @@
 // Selecting elements
 const score0Element = document.querySelector('#score--0');
 const score1Element = document.querySelector('#score--1');
+const current0Element = document.getElementById('current--0');
 const diceElement = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -12,3 +13,21 @@ const btnHold = document.querySelector('.btn--hold');
 score0Element.textContent = 0;
 score1Element.textContent = 0;
 diceElement.classList.add('hidden');
+
+let currnetScore = 0;
+
+btnRoll.addEventListener('click', function () {
+    // 1. Generating a random dice roll
+    const diceNumber = Math.trunc(Math.random() * 6) + 1;
+
+    // 2. Display dice
+    diceElement.classList.remove('hidden');
+    diceElement.src = `img/dice${diceNumber}.png`;
+
+    // 3. Check for rolled 1
+    if (diceNumber !== 1) {
+        // Finish the game
+        currnetScore += diceNumber;
+        do
+    }
+})
