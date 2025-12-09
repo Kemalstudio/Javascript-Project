debfd74b2361ba013bc05fab36a7046addba94c9
Kemal Atayev <kemalatayew913@gmail.com>
2025-12-09 19:46:44 +0500

    3.99

--- Game/script.js
+++ Game/script.js
@@ -4,6 +4,9 @@
 const score0Element = document.querySelector('#score--0');
 const score1Element = document.querySelector('#score--1');
 const diceElement = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 // Starting conditions
 score0Element.textContent = 0;
