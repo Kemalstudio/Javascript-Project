a3cb40eaae798dca2b149719439a1a9b76b79c9c
Kemal Atayev <kemalatayew913@gmail.com>
2025-12-09 21:37:02 +0500

    4.9

--- Game/script.js
+++ Game/script.js
@@ -9,6 +9,8 @@ const diceElement = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+const player0Element = document.querySelector('.player--0');
+const player1Element = document.querySelector('.player--1');
 
 // Starting conditions
 score0Element.textContent = 0;
@@ -35,6 +37,32 @@ btnRoll.addEventListener('click', function () {
     } else {
         currnetScore = 0;
         document.getElementById(`current--${activePlayer}`).textContent = currnetScore;
-        activePlyayer = activePlayer === 0 ? 1 : 0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        player0Element.classList.toggle('player--active');
+        player1Element.classList.toggle('player--active');
+
     }
 })
+
+btnHold.addEventListener('click', function () {
+    // 1. Add current score to active player's score
+    totalScores[activePlayer] += currnetScore;
+    document.getElementById(`score--${activePlayer}`).textContent = totalScores[activePlayer];
+
+    // 2. Check if player's score is >=100
+    if (totalScores[activePlayer] >= 100) {
+        // Finish the game
+        document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+        document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+        diceElement.classList.add('hidden');
+        btnRoll.disabled = true;
+        btnHold.disabled = true;
+    } else {
+        // Switch to the next player
+        currnetScore = 0;
+        document.getElementById(`current--${activePlayer}`).textContent = currnetScore;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        player0Element.classList.toggle('player--active');
+        player1Element.classList.toggle('player--active');
+    }
+});
