c260a9863068a319e22fcb50686ec3858c72a057
Kemal Atayev <kemalatayew913@gmail.com>
2025-12-10 17:19:20 +0500

    4.24

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 'use strict';
 
-functiongetAge(birthYear) {
+function getAge(birthYear) {
     const age = 2024 - birthYear;
+    console.log(name);
     return age;
-    
 }
+
+const name = 'Kemal';
+getAge(2011);
