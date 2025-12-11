692861ae0929c25dcfcc8f88b0bdb1f9dfaaf768
Kemal Atayev <kemalatayew913@gmail.com>
2025-12-11 20:48:31 +0500

    4.53

--- изучение/script.js
+++ изучение/script.js
@@ -6,18 +6,18 @@
 // getAge(2001);
 
 
-// const getAgeArr = function (birthYear) {
-//     console.log(2021 - birthYear)
-//     console.log(this);
-// };
+const getAgeArr = function (birthYear) {
+    console.log(2021 - birthYear)
+    console.log(this);
+};
 
-// getAgeArr(2001);
+getAgeArr(2001);
 
-// const user187 = {
-//     birthYear: 1974,
-//     getAge: function () {
-//         console.log(this);
-//     },
-// };
+const user187 = {
+    birthYear: 1974,
+    getAge: function () {
+        console.log(this);
+    },
+};
 
-// user187.getAge();
+user187.getAge();
