3c07713d6b4d5cb4c38bdcfbd0c9041c5bf66edb
Kemal Atayev <kemalatayew913@gmail.com>
2025-12-12 20:44:37 +0500

    5.68

--- изучение/script.js
+++ изучение/script.js
@@ -1,13 +1,17 @@
 'use strict';
 
-const booking = [];
+const bookings = [];
 
-const makeBooking = function(flightNum, passengerName, price) {
-    const booking ={
+const makeBooking = function(flightNum, passengersNum, price) {
+
+    
+    const booking = {
         flightNum, 
-        passengerName, 
-        price
+        passengersNum, 
+        price,
     };
     console.log(booking);
-    booking.push(booking);
-}
+    bookings.push(booking);
+};
+
+makeBooking('QE123')
