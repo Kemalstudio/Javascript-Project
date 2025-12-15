8fb0725000e00c9e408c345f0a52eab170a7ff85
Kemal Atayev <kemalatayew913@gmail.com>
2025-12-15 23:53:54 +0500

    6.70

--- изучение/script.js
+++ изучение/script.js
@@ -1,42 +1,49 @@
-'use strict';
+// 'use strict';
 
-const bookings = [];
+// const bookings = [];
 
-const makeBooking = function(flightNum, passengersNum = 1, price = 99 * passengersNum) {
-    // Before ES6 default parameters
-    // passengersNum = passengersNum || 1;
-    // price = price || 99;
+// const makeBooking = function(flightNum, passengersNum = 1, price = 99 * passengersNum) {
+//     // Before ES6 default parameters
+//     // passengersNum = passengersNum || 1;
+//     // price = price || 99;
 
-    const booking = {
-        flightNum, 
-        passengersNum, 
-        price,
-    };
-    console.log(booking);
-    bookings.push(booking);
-};
+//     const booking = {
+//         flightNum, 
+//         passengersNum, 
+//         price,
+//     };
+//     console.log(booking);
+//     bookings.push(booking);
+// };
 
-makeBooking('QE123');
-makeBooking('TU235', 3, 390);
-makeBooking('QE123', 1);
-makeBooking('QE123', undefined, 400);
+// makeBooking('QE123');
+// makeBooking('TU235', 3, 390);
+// makeBooking('QE123', 1);
+// makeBooking('QE123', undefined, 400);
 
-const checkIn = function(flightNum, passenger) {
-    flightNum = 'bv328';
-    passenger.firstName = passenger.firstName.toLowerCase();
-    passenger.lastName = passenger.lastName.toLowerCase();
-    if (passenger.passport === HF128490) {
-        alert('Welcome to the board');
-    } else {
-        alert('Wrong passport');
-    }
-};
+// const checkIn = function(flightNum, passenger) {
+//     flightNum = 'bv328';
+//     passenger.firstName = passenger.firstName.toLowerCase();
+//     passenger.lastName = passenger.lastName.toLowerCase();
+//     if (passenger.passport === HF128490) {
+//         alert('Welcome to the board');
+//     } else {
+//         alert('Wrong passport');
+//     }
+// };
 
-checkIn(flightNum, passenger1145);
-console.log(flightNum);
-console.log(passenger1145);
+// checkIn(flightNum, passenger1145);
+// console.log(flightNum);
+// console.log(passenger1145);
 
-const flight = flightNumber;
-const passenger = passenger1145;
+// const flight = flightNumber;
+// const passenger = passenger1145;
 
-changePasswordNumber
+// changePasswordNumber
+
+
+book.call(airline2, 345, 'Linda Wilams');
+console.log(airline2);
+
+boock.call(airline, 111, 'Bob Smitsh');
+console.log(airline1)
