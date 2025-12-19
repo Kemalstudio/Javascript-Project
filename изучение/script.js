76b512c74003c97d996844707d3af77dbffd3b91
Kemal Atayev <kemalatayew913@gmail.com>
2025-12-20 00:14:34 +0500

    9.45

--- изучение/script.js
+++ изучение/script.js
@@ -1,54 +1,11 @@
-// 'use strict';
+const userName = 'Kemal Atayew';
+const nickname = userName
+.toLowerCase()
+.split(' ')
+.map(function (word) {
+    return word[0];
+})
+.join('');
 
-// const bookings = [];
-
-// const makeBooking = function(flightNum, passengersNum = 1, price = 99 * passengersNum) {
-//     // Before ES6 default parameters
-//     // passengersNum = passengersNum || 1;
-//     // price = price || 99;
-
-//     const booking = {
-//         flightNum, 
-//         passengersNum, 
-//         price,
-//     };
-//     console.log(booking);
-//     bookings.push(booking);
-// };
-
-// makeBooking('QE123');
-// makeBooking('TU235', 3, 390);
-// makeBooking('QE123', 1);
-// makeBooking('QE123', undefined, 400);
-
-// const checkIn = function(flightNum, passenger) {
-//     flightNum = 'bv328';
-//     passenger.firstName = passenger.firstName.toLowerCase();
-//     passenger.lastName = passenger.lastName.toLowerCase();
-//     if (passenger.passport === HF128490) {
-//         alert('Welcome to the board');
-//     } else {
-//         alert('Wrong passport');
-//     }
-// };
-
-// checkIn(flightNum, passenger1145);
-// console.log(flightNum);
-// console.log(passenger1145);
-
-// const flight = flightNumber;
-// const passenger = passenger1145;
-
-// changePasswordNumber
-
- 
-book.call(airline2, 345, 'Linda Wilams');
-console.log(airline2);
-
-boock.call(airline, 111, 'Bob Smitsh');
-console.log(airline1)
-
-
-// boock.call(airline, 111, 'Bob  
-
-const verify6cats = f
+console.log(nickname);
+console.log(nickname).join()
