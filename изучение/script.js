63b3aced9a51ecdf864dc72db316c9a491caf3f9
Kemal Atayev <kemalatayew913@gmail.com>
2025-12-20 21:34:04 +0500

    9.46

--- изучение/script.js
+++ изучение/script.js
@@ -9,3 +9,16 @@ const nickname = userName
 
 console.log(nickname);
 console.log(nickname).join()
+console.log('------------------------------')
+// 
+// const userName = 'Kemal Atayew';
+// const nickname = userName
+// .toLowerCase()
+// .split(' ')
+// .map(function (word) {
+//     return word[0];
+// })
+.join('');
+
+console.log(nickname);
+console.log(nickname).join()
