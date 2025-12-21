3294935041b1fae88426420665c0fbdfc1d0df4e
Kemal Atayev <kemalatayew913@gmail.com>
2025-12-21 13:35:56 +0500

    9.88

--- изучение/script.js
+++ изучение/script.js
@@ -1,5 +1,5 @@
 // Display UI and welcome message
-// Display UI and w
+// Display UI and 
 
 butonLogin.addEventListener('click', function() {
     const username = document.getElementById('username').value;
