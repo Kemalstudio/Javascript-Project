2fb9a13ee31ce709855e76354c770d0bceece097
Kemal Atayev <kemalatayew913@gmail.com>
2025-12-21 14:37:18 +0500

    10.20

---
+++ project-1/script.js
@@ -0,0 +1,52 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.number-input').value);
+
+  console.log(guess, typeof guess);
+
+  // Когда игрок ничего не ввёл
+  if (!guess) {
+    document.querySelector('.guess-message').textContent = 'Нет числа!';
+
+  } else if (guess === secretNumber) {
+    document.querySelector('.guess-message').textContent = 'Правильно!';
+    document.querySelector('.question').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.question').style.width = '50rem';
+
+    // Обновляем
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
+    // Когда не угадал
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      document.querySelector('.guess-message').textContent =
+        guess > secretNumber ? 'Слишком много!' : 'Слишком мало!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.guess-message').textContent = 'Game Over!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector('.question').textContent = '???';
+  document.querySelector('.question').style.width = '25rem';
+  document.querySelector('.guess-message').textContent = 'Начни угадывать...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number-input').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+});
