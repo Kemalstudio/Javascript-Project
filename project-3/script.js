492f251e5fd1c604e0830f63e001753da5a1615b
Kemal Atayev <kemalatayew913@gmail.com>
2025-12-23 23:58:42 +0500

    11.96

--- project-3/script.js
+++ project-3/script.js
@@ -162,7 +162,7 @@ btnLogin.addEventListener('click', function (e) {
   console.log(currentAccount);
 
   if (currentAccount?.pin === Number(inputLoginPin.value)) {
-    // 
+    //
     containerApp.style.opacity = 100;
 
     labelWelcome.textContent = `Рады, что вы снова с нами, ${
