0c4228fc338d100de55648c7a48b940e1536ae29
Kemal Atayev <kemalatayew913@gmail.com>
2025-12-24 22:50:42 +0500

    12.9

--- project-3/script.js
+++ project-3/script.js
@@ -154,7 +154,7 @@ const updateUi = function (account) {
 let currentAccount;
 
 // Event Handlers
-// Eve
+// Ev
 btnLogin.addEventListener('click', function (e) {
   e.preventDefault();
   currentAccount = accounts.find(
