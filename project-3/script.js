f2c81421fb5a1ce7b4cbbc9c967a312b8fe90208
Kemal Atayev <kemalatayew913@gmail.com>
2025-12-26 23:27:32 +0500

    13.29

--- project-3/script.js
+++ project-3/script.js
@@ -86,7 +86,7 @@ const displayTransactions = function (transactions, sort = false) {
 };
 
 
-// console.log(containe
+// console.log(contain
 
 const createNicknames = function (accs) {
   accs.forEach(function (acc) {
