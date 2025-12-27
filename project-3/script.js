ff04e663db14fb550ef34053645909433619c2a7
Kemal Atayev <kemalatayew913@gmail.com>
2025-12-27 20:42:30 +0500

    14.18

--- project-3/script.js
+++ project-3/script.js
@@ -139,7 +139,7 @@ const updateUi = function (account) {
   // Display transactions
   displayTransactions(account.transactions);
 
-  // document.querySelecto
+  // document.querySelect
   displayBalance(account);
 
   displayTotal(account);
