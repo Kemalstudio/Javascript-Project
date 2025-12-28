ef62c4339a361529dc29836e78cb5a3b3fd3abba
Kemal Atayev <kemalatayew913@gmail.com>
2025-12-28 12:46:33 +0500

    14.76

--- project-5/script.js
+++ project-5/script.js
@@ -17,7 +17,7 @@ const nav = document.querySelector('.nav');
 
 ///////////////////////////////////////
 // Modal window
-// Mod
+// Mo
 
 const openModalWindow = function (e) {
   e.preventDefault();
