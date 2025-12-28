acb7e0cb54700e9e978ce8dd8ae987012e839ff8
Kemal Atayev <kemalatayew913@gmail.com>
2025-12-29 00:11:39 +0500

    15.13

--- project-5/script.js
+++ project-5/script.js
@@ -535,4 +535,4 @@ window.addEventListener('load', function (e) {
 // });
 
 // const tabs = document.querySelectorAll('.operations__tab');
-// const tabContainer = document.querySelector('.operat
+// const tabContainer = document.querySelector('.opera
