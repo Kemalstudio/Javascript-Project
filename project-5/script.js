4a1aba08448d3698e07f0d47257aca96acf7340a
Kemal Atayev <kemalatayew913@gmail.com>
2025-12-30 21:01:38 +0500

    15.67

--- project-5/script.js
+++ project-5/script.js
@@ -198,7 +198,7 @@ const headerObserver = new IntersectionObserver(getStickyNav, {
 });
 headerObserver.observe(header);
 
-// Появлени
+// Появлен
 
 const allSections = document.querySelectorAll('.section');
 
