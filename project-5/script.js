cffacb3aa8a27b198bc582f272e34d823f12987d
Kemal Atayev <kemalatayew913@gmail.com>
2025-12-30 21:43:45 +0500

    15.82

--- project-5/script.js
+++ project-5/script.js
@@ -176,7 +176,7 @@ nav.addEventListener('mouseout', navLinksHoverAnimation.bind(1));
 // };
 
 // const observer = new IntersectionObserver(observerCallback, observerOptions);
-// observer.observe(section1
+// observer.observe(section
 
 const header = document.querySelector('.header');
 const navHeight = nav.getBoundingClientRect().height;
