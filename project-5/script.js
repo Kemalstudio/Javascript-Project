b156a3114327cd039950ee898b9acf602c1b9b85
Kemal Atayev <kemalatayew913@gmail.com>
2025-12-31 12:34:32 +0500

    16.4

--- project-5/script.js
+++ project-5/script.js
@@ -218,7 +218,7 @@ allSections.forEach(function (section) {
   section.classList.add('section--hidden');
 });
 
-// Имплементация lazy loadi
+// Имплементация lazy load
 const lazyImages = document.querySelectorAll('img[data-src]');
 
 const loadImages = function (entries, observer) {
