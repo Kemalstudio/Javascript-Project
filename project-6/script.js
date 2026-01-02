84c6616914a8d0bacc0e2158ffacb61259150765
Kemal Atayev <kemalatayew913@gmail.com>
2026-01-02 21:04:25 +0500

    16.82

--- project-6/script.js
+++ project-6/script.js
@@ -16,7 +16,7 @@ class Workout {
   constructor(coords, distance, duration) {
     this.coords = coords;
     this.distance = distance; // km
-    this.duration = duration; // min
+    this.duration = duration; // mi
   }
 
   _setDescription() {
