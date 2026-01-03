f81e01f9f29f3d8d8662cd991d2f96d5b1b3489b
Kemal Atayev <kemalatayew913@gmail.com>
2026-01-04 00:11:11 +0500

    18.62

--- project-6/script.js
+++ project-6/script.js
@@ -200,7 +200,7 @@ class App {
     // Отобразить тренировку в списке
     this._displayWorkoutOnSidebar(workout);
 
-    // Спрятать форму и очистить поля
+    // Спрятать форму и очистить пол
     this._hideForm();
     this._addWorkoutsToLocalStorage();
   }
