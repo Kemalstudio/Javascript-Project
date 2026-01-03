8d3625782fa6a0d5ed744cbc0a14169e711a35ca
Kemal Atayev <kemalatayew913@gmail.com>
2026-01-03 23:55:54 +0500

    17.22

--- project-6/script.js
+++ project-6/script.js
@@ -203,7 +203,7 @@ class App {
     // Спрятать форму и очистить поля ввода данных
     this._hideForm();
 
-    // Добавить все трениро
+    // Добавить все тренир
     this._addWorkoutsToLocalStorage();
   }
 
