bbfc326c2b05a9966f42d7f611b90d2c835b3114
Kemal Atayev <kemalatayew913@gmail.com>
2026-01-04 13:49:17 +0500

    19.2

--- project-6/script.js
+++ project-6/script.js
@@ -117,7 +117,7 @@ class App {
     // Обработка клика на карте
     this.#map.on('click', this._showForm.bind(this));
 
-    // Отображение тренировок из local storage н
+    // Отображение тренировок из local storage 
     this.#workouts.forEach(workout => {
       this._displayWorkout(workout);
     });
