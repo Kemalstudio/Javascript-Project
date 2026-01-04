44bdcdb1ba120c2ff52f574e0f16fd3c9dc15325
Kemal Atayev <kemalatayew913@gmail.com>
2026-01-04 23:13:05 +0500

    19.21

--- project-6/script.js
+++ project-6/script.js
@@ -114,7 +114,7 @@ class App {
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(this.#map);
 
-    // Обработка клика на
+    // Обработка клика н
     this.#map.on('click', this._showForm.bind(this));
 
     // Отображение тренировок из lo
