8a43ba15da24f390054bd003fd4f2590a072907d
Kemal Atayev <kemalatayew913@gmail.com>
2026-01-05 19:30:42 +0500

    19.52

--- project-6/script.js
+++ project-6/script.js
@@ -81,7 +81,7 @@ class App {
     // Получение данных из local storage
     this._getLocalStorageData();
 
-    // Добавление обработчиков собы
+    // Добавление обработчиков соб
     form.addEventListener('submit', this._newWorkout.bind(this));
     inputType.addEventListener('change', this._toggleClimbField);
     containerWorkouts.addEventListener('click', this._moveToWorkout.bind(this));
