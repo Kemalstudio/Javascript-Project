eb272619003f285e8825d26c1ff5d77da0b9e8aa
Kemal Atayev <kemalatayew913@gmail.com>
2026-01-06 23:47:09 +0500

    20.24

--- project-6/script.js
+++ project-6/script.js
@@ -112,7 +112,7 @@ class App {
 
     this.#map.on('click', this._showForm.bind(this));
 
-    // Отображе
+    // Отображ
     this.#workouts.forEach(workout => {
       this._displayWorkout(workout);
     });
