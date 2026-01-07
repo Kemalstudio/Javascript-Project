906d21fb09b8f9e4cf8bcf5f7902b3fb79cf33fa
Kemal Atayev <kemalatayew913@gmail.com>
2026-01-07 16:51:06 +0500

    21.27

--- project-7/script.js
+++ project-7/script.js
@@ -668,7 +668,7 @@ const wait = function (seconds) {
 
 const imageContainer = document.querySelector('.images');
 
-// let current
+// let curren
 
 const createImageElement = function (imagePath) {
   return new Promise(function (resolve, reject) {
