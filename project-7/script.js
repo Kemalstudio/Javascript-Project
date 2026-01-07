a2f5b334cefe9c3a1257dee9e36f1afd44aa78df
Kemal Atayev <kemalatayew913@gmail.com>
2026-01-07 13:05:48 +0500

    20.94

--- project-7/script.js
+++ project-7/script.js
@@ -112,7 +112,7 @@ const getDataAndConvertToJSON = function (
 //   getCoutnryData('ukraine');
 // });
 
-// import cloneDeep from ./node_modules/-lodash-esh clone
+// import cloneDeep from ./node_modules/-lodash-esh clon
 
 // const getCoutnryData = function (countryName) {
 //   const request = new XMLHttpRequest();
