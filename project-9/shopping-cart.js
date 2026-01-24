2ef1bb67308d8eddfc1616a1146817d286847c48
Kemal Atayev <kemalatayew913@gmail.com>
2026-01-25 00:19:37 +0500

    32.37

--- project-9/shopping-cart.js
+++ project-9/shopping-cart.js
@@ -26,4 +26,4 @@ export default function (product, quantity) {
   console.log(`${product} в количестве ${quantity} шт добавлено в корзину`);
 }
 
-// export const greet = functio
+// export const greet = functi
