6fff19c9d4b720254ee18626451e650170d6bdf3
Kemal Atayev <kemalatayew913@gmail.com>
2026-01-27 11:10:16 +0500

    33.60

--- project-9/shopping-cart.js
+++ project-9/shopping-cart.js
@@ -26,4 +26,4 @@ export default function (product, quantity) {
   console.log(`${product} в количестве ${quantity} шт добавлено в корзину`);
 }
 
-// import React, {useEffect, useState} from 
+// import React, {useEffect, useState} from
