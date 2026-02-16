43c320d38b4f0a10d3f415658640e535aa1f8518
Kemal Atayev <kemalatayew913@gmail.com>
2026-02-16 23:12:50 +0500

    50.92

--- project-9/script.js
+++ project-9/script.js
@@ -7,4 +7,4 @@ export function fetchData() {
             const data = await res.json();
             dispatch({type: "userList/fetchDataSuccess", payload: data});
         } catch (error {
-            dispatch({type: "userList/fetchDataFailure", payload: error.message
+            dispatch({type: "userList/fetchDataFailure", payload: error.messag
