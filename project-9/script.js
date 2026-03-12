ad1b1806ff5ec78aee522381498d3ad649d20314
Kemal Atayev <kemalatayew913@gmail.com>
2026-03-12 23:53:40 +0500

    53.7

--- project-9/script.js
+++ project-9/script.js
@@ -14,4 +14,4 @@ export function fetchData() {
 
 // export function fetchData() {
 //     return function (dispatch) {
-//         dispatch({type: "userList/fetchData"}
+//         dispatch({type: "userList/fetchData"
