7161a5067d698f5cbd486ad4540f144070ac84a9
Kemal Atayev <kemalatayew913@gmail.com>
2026-03-13 22:18:28 +0500

    54.24

--- project-9/script.js
+++ project-9/script.js
@@ -11,3 +11,18 @@ export function fetchData() {
         }
     }
 }
+
+
+// export function fetchData() {
+//     return async function (dispatch, getState) {
+//         console.log(getState());
+//         dispatch({type: "userList/fetchData"});
+//         try {
+//             const res = await fetch("https://jsonplaceholder.typicode.com/users");
+//             const data = await res.json();
+//             dispatch({type: "userList/fetchDataSuccess", payload: data});
+//         } catch (error) {
+//             dispatch({type: "userList/fetchDateError", payload: error.message});        }
+//         }
+//     }
+// 
