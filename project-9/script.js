89d10b88fe6bd9156223b08ff968ceb0f6407a1b
Kemal Atayev <kemalatayew913@gmail.com>
2026-03-19 16:22:45 +0500

    57.34

--- project-9/script.js
+++ project-9/script.js
@@ -12,9 +12,22 @@ export function fetchData() {
     }
 }
 
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+const ambientLight = new THREE.AmbientLight('#fff');
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight('#fff', 1);
+directionalLight.position.set(0.25, 3, -2.25);
+scene.add(directionalLight);
+
+const gltfLoader = new GLTFLoader();
+
+
 // export function fetchData() {
 //     return async function (dispatch, getState) {
 //         console.log(getState());1
 //         dispatch({type: "userList/fetchData"});
 //         try {
-//             const res = await fetch("https://jsonplaceholder.typicode.c
+//             const res = await fetch("https://jsonplaceholder.typicode.
