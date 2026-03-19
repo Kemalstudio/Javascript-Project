a75f046c37585c4ad4196909ca3955f4d8bf1eba
Kemal Atayev <kemalatayew913@gmail.com>
2026-03-19 17:27:27 +0500

    57.81

--- project-9/script.js
+++ project-9/script.js
@@ -30,5 +30,5 @@ const gltfLoader = new GLTFLoader();
 //         console.log(getState());1
 //         dispatch({type: "userList/fetchData"});
 //         try {
-//             const res = aw
+//             const res = a
 const gLoader = new gltfLoader()
