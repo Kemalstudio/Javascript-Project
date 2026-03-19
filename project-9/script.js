94ff4e13d462ae7e44bce8d497d97a08cd858203
Kemal Atayev <kemalatayew913@gmail.com>
2026-03-19 16:17:29 +0500

    57.28

--- project-9/script.js
+++ project-9/script.js
@@ -17,4 +17,4 @@ export function fetchData() {
 //         console.log(getState());1
 //         dispatch({type: "userList/fetchData"});
 //         try {
-//             const res = await fetch("https://jsonplaceholder.typicode.com/use
+//             const res = await fetch("https://jsonplaceholder.typicode.com/us
