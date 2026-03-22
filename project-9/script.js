eb127c7b824b857d54573ee311b8e38b8fdd9945
Kemal Atayev <kemalatayew913@gmail.com>
2026-03-22 15:07:54 +0500

    60.1

--- project-9/script.js
+++ project-9/script.js
@@ -26,4 +26,4 @@ const gltfLoader = new GLTFLoader();
 
 // export function fetchData() {
 
-//     return async function (dis -+                                                                                     
+//     return async function (dis -+                                                                                    
