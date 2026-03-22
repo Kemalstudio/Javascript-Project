bf2703793857b16a7958595f695d20c0bd1d3dfd
Kemal Atayev <kemalatayew913@gmail.com>
2026-03-22 20:59:28 +0500

    60.52

--- project-8/script.js
+++ project-8/script.js
@@ -23,4 +23,4 @@
 
 // export default function (product, quantity) {
 //   cart.push({ product, quantity });
-//   console.log(`${product} в количестве ${quantity} шт добавлено в корзину`);
+//   console.log(`${product} в количестве ${quantity} шт добавлено в корзину`)
