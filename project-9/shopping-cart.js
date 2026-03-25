2c9d81d9aa2eec8c10db79deabc6302b0258c584
Kemal Atayev <kemalatayew913@gmail.com>
2026-03-25 14:32:44 +0500

    61.61

--- project-9/shopping-cart.js
+++ project-9/shopping-cart.js
@@ -1,27 +1,27 @@
-// // Экспорт модуля
-// console.log('Экспорт модуля');
+// Экспорт модуля
+console.log('Экспорт модуля');
 
-// // Блокирующий код
-// // console.log('Начало извлечения пользователей');
-// // const result = await fetch('https://jsonplaceholder.typicode.com/users');
-// // console.log('Конец извлечения пользователей');
+// Блокирующий код
+// console.log('Начало извлечения пользователей');
+// const result = await fetch('https://jsonplaceholder.typicode.com/users');
+// console.log('Конец извлечения пользователей');
 
-// const shippingCost = 20;
-// export const cart = [];
+const shippingCost = 20;
+export const cart = [];
 
-// export const 
+export const 
 
-// addProductToCart = function (product, quantity) {
-//   cart.push({ product, quantity });
-//   console.log(`${product} в количестве ${quantity} шт добавлено в корзину`);
-// };
+addProductToCart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${product} в количестве ${quantity} шт добавлено в корзину`);
+};
 
-// const totalPrice = 250;
-// const totalQuantity = 7;
+const totalPrice = 250;
+const totalQuantity = 7;
 
-// export { totalPrice, totalQuantity as quantity };
+export { totalPrice, totalQuantity as quantity };
 
-// export default function (product, quantity) {
-//   cart.push({ product, quantity });
-//   console.log(`${product} в количестве ${quantity} шт добавлено в корзину`);
-// }
+export default function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${product} в количестве ${quantity} шт добавлено в корзину`);
+}
