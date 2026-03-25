4daf9b495cae0ae5501428a4c612a131f12301b0
Kemal Atayev <kemalatayew913@gmail.com>
2026-03-25 20:53:50 +0500

    61.71

--- project-9/shopping-cart.js
+++ project-9/shopping-cart.js
@@ -4,7 +4,7 @@ console.log('Экспорт модуля');
 // Блокирующий код
 // console.log('Начало извлечения пользователей');
 // const result = await fetch('https://jsonplaceholder.typicode.com/users');
-// console.log('Конец извлечения пользовате
+// console.log('Конец извлечения пользоват
 
 const shippingCost = 20;
 export const cart = [];
